3cb6d265378e371b213067fd8f31ba6ea3338441
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-09-18 21:25:57 -0600

    feat: add investor model

---
+++ InvestorService/Includes/Models/InvestorModel.hpp
@@ -0,0 +1,21 @@
+//
+//  Created by Jeremy Fonseca on September, 2022
+//
+
+#ifndef D5285D56_6C56_449E_BA32_F5E9CBB7B3C3
+#define D5285D56_6C56_449E_BA32_F5E9CBB7B3C3
+
+#include <string>
+
+namespace Server::Models
+{
+  struct InvestorModel
+  {
+    std::string Name;
+    std::string Email;
+    std::string Phone;
+  };
+
+}
+
+#endif /* D5285D56_6C56_449E_BA32_F5E9CBB7B3C3 */
