53fc7cae94986704c250ad9b84d660670d622618
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-09-19 15:14:40 -0600

    feat: add user model

---
+++ AuthenticationService/Includes/Models/UserModel.hpp
@@ -0,0 +1,16 @@
+#ifndef FE7022B4_CD78_4446_BFF7_FA91790AE9B7
+#define FE7022B4_CD78_4446_BFF7_FA91790AE9B7
+
+#include <string>
+
+namespace Server::Models
+{
+  struct UserModel
+  {
+    std::string Email;
+    std::string Password;
+  };
+
+}
+
+#endif /* FE7022B4_CD78_4446_BFF7_FA91790AE9B7 */
