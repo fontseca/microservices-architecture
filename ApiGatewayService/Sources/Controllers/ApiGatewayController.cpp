e056160609a9af1272e96cfd9e6303ebebc169cd
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-09-19 21:59:45 -0600

    feat: add auth routes to api gateway

--- ApiGatewayService/Sources/Controllers/ApiGatewayController.cpp
+++ ApiGatewayService/Sources/Controllers/ApiGatewayController.cpp
@@ -5,7 +5,10 @@
 #include <sstream>
 
 #include <cpprest/http_listener.h>
+#include <cpprest/http_client.h>
+
 #include <bsoncxx/json.hpp>
+
 #include <jwt/jwt.hpp>
 
 #include "Includes/Controllers/ApiGatewayController.hpp"
@@ -22,6 +25,45 @@ void ApiGatewayController::HandleGet(http_request request)
 void ApiGatewayController::HandlePost(http_request request)
 {
   Server::Core::BaseController::HandlePost(request);
+  const auto path = this->RequestPath(request);
+
+  if (path.empty())
+    request.reply(status_codes::NotFound);
+
+  if (path[0] == "auth" && path.size() == 2)
+  {
+    if (path[1] == "signup" || path[1] == "login")
+    {
+
+      const auto request_to_auth_service = [=](pplx::task<web::json::value> task)
+      {
+        client::http_client auth_client{U("http://localhost:5001/" + path[1])};
+        web::json::value user_json = task.get();
+        return auth_client.request(web::http::methods::POST, U(""), user_json.serialize(), U("application/json"));
+      };
+
+      const auto get_auth_service_response = [=](pplx::task<http_response> task)
+      {
+        auto response = task.get();
+        return response.extract_json();
+      };
+
+      const auto reply_auth_service_response = [=](pplx::task<web::json::value> task)
+      {
+        auto res = task.get();
+        return request.reply(status_codes::OK, res);
+      };
+
+      request
+          .extract_json()
+          .then(request_to_auth_service)
+          .then(get_auth_service_response)
+          .then(reply_auth_service_response);
+      return;
+    }
+  }
+  request.reply(status_codes::NotFound);
+  return;
 }
 
 void ApiGatewayController::HandlePut(http_request request)
